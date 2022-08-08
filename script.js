d8d110113cc2b9d253ac3400c80677a5fde721fe
Astralium <astralium24@gmail.com>
2022-08-08 23:14:52 +0700

    Added HTM, CS, JS, and other stuff
    Uploading my website stuff to the repo

---
+++ script.js
@@ -0,0 +1,8 @@
+var myArray = {
+    "User": "admin",
+    "Password": "admin"
+};
+function myAlert(){
+    var output = "User: " + myArray.User + "\n" + "Password: " + myArray.Password;
+    alert(output);
+}
