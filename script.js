89f9e3fd744c7d2c2cf830c58c734e4e58e2b434
Astralium <astralium24@gmail.com>
2022-08-09 02:27:13 +0700

    JavaScript update and website update

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function myAlert(){
     var output = "User: " + myArray.User + "\n" + "Password: " + myArray.Password;
     alert(output);
 }
+function myColor(){
+    document.getElementById("div3").style.backgroundColor = "#141619";
+}
+function myColor2(){
+    document.getElementById("div3").style.backgroundColor = "#2E3339";
+}
