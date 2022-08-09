50939ce02a14666a76a4a3c41be1f06c56c57566
Astralium <astralium24@gmail.com>
2022-08-09 15:40:15 +0700

    Updating JS, HTML, and CSS files

--- script.js
+++ script.js
@@ -2,13 +2,34 @@ var myArray = {
     "User": "admin",
     "Password": "admin"
 };
+
 function myAlert(){
-    var output = "User: " + myArray.User + "\n" + "Password: " + myArray.Password;
-    alert(output);
+  var output = "User: " + myArray.User + "\n" + "Password: " + myArray.Password;
+  alert(output);
 }
 function myColor(){
-    document.getElementById("div3").style.backgroundColor = "#141619";
+  document.getElementById("div3").style.backgroundColor = "#141619";
 }
 function myColor2(){
-    document.getElementById("div3").style.backgroundColor = "#2E3339";
+  document.getElementById("div3").style.backgroundColor = "#2E3339";
+}
+
+window.onscroll = function() {myHeaderState()};
+function myHeaderState(){
+  const block = document.getElementById("header-top");
+  const header = document.getElementById("header-bottom");
+  const offSet = header.offsetTop;
+
+  if (window.pageYOffset > offSet){
+    console.log(window.pageYOffset);
+    console.log(offSet);
+
+    header.style.position = "fixed";
+    header.style.top = 0;
+    block.style.paddingBottom = "90px";
+  } 
+  if (window.pageYOffset < 37){
+    header.style.position = "static";
+    block.style.paddingBottom = "10px";
+  }
 }
